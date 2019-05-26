e8942bbc890759ca4d382da15a441b6d90267f64
nicwang118 <49328353+nicwang118@users.noreply.github.com>
2019-05-26 18:48:27 -0400

    Update mymap.js

--- mymap.js
+++ mymap.js
@@ -1,4 +1,6 @@
-var map;
-function loadMapScenario() {
-    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
-}
+
+
+                var map;
+                function loadMapScenario() {
+                    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
+                }
